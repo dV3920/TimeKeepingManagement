feb3d8278a7d9e9d8e9373194a2dfaad5eea758b
magnetohvcs <ttrungt1742@gmail.com>
2022-03-29 16:18:26 +0700

    add product

--- app/src/main/java/com/example/timekeepingmanagement/ProductActivity.java
+++ app/src/main/java/com/example/timekeepingmanagement/ProductActivity.java
@@ -2,8 +2,12 @@ package com.example.timekeepingmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +17,7 @@ public class ProductActivity extends AppCompatActivity {
     DataBase db;
     ArrayList<Product> data = new ArrayList<>();
     ProductAdapter productAdapter;
+    ImageView ivAddProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +28,29 @@ public class ProductActivity extends AppCompatActivity {
     }
 
     void  setControl(){
-
+        db = new DataBase(getApplicationContext());
+        lvListProduct = findViewById(R.id.lvListProduct);
+        ivAddProduct = findViewById(R.id.ivAddProduct);
     }
 
     void setEvent(){
         init();
         productAdapter = new ProductAdapter(this,R.layout.raw_product,data);
         lvListProduct.setAdapter(productAdapter);
+        ivAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProductActivity.this, AddProductActivity.class);
+                intent.putExtra("isAdd",true);
+                startActivity(intent);
+            }
+        });
     }
 
     void init(){
-        data.addAll(db.readProducts());
+        data.addAll(db.readProducts()); //new Product(1,"Thép",1000)
     }
+
+
 
 }

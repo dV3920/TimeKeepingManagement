93e2b0641087e2408bc4c0bd1ca87bff93a1b8b1
magnetohvcs <ttrungt1742@gmail.com>
2022-03-30 15:00:51 +0700

    Complete Product

--- app/src/main/java/com/example/timekeepingmanagement/ProductAdapter.java
+++ app/src/main/java/com/example/timekeepingmanagement/ProductAdapter.java
@@ -1,6 +1,9 @@
 package com.example.timekeepingmanagement;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -43,7 +47,40 @@ public class ProductAdapter extends ArrayAdapter {
         tvId.setText(product.getId()+" ");
         tvName.setText(product.getName());
         tvPrice.setText(product.getPrice()+"");
+        ImageView ivEditProduct = convertView.findViewById(R.id.ivEditProduct);
+        ImageView ivRemoveProduct = convertView.findViewById(R.id.ivRemoveProduct);
+        ivEditProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, AddProductActivity.class);
+                intent.putExtra("isAdd",false);
+                intent.putExtra("Object",product);
+                context.startActivity(intent);
+            }
+        });
+        ivRemoveProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder adb = new AlertDialog.Builder(context);
+                adb.setMessage("Bạn có muốn xóa sản phẩm có mã là "+ product.getId());
+                adb.setTitle("Thông báo");
+                adb.setPositiveButton("Đồng Ý", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Boolean isSuccess =  new DataBase(context).removeProduct(product.getId());
+                        if(isSuccess){
+                            Toast.makeText(context,"Xóa thành công",Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(context,"Lỗi",Toast.LENGTH_LONG).show();
+                        }
+                        data.remove(product);
+                        notifyDataSetChanged();
+                    }
+                });
+                adb.setNegativeButton("Hủy",null);
+                adb.show();
 
+            }
+        });
 
         return convertView;
     }

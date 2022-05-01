1ab07440558c2b72e257878d4be44f05cb7bd3cd
boiprcvn3 <hdinhvu156@gmail.com>
2022-05-01 15:24:43 +0700

    vu push

---
+++ app/src/main/java/com/example/timekeepingmanagement/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.timekeepingmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    Button btnSignUp;
+    EditText txtUserName;
+    EditText txtPasswd;
+    DataBase database;
+    SQLiteDatabase TimeKeepingDb;
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        txtUserName = (EditText) findViewById(R.id.txtUsername);
+        txtPasswd = (EditText) findViewById(R.id.txtPassword);
+        database = new DataBase(getApplicationContext());
+        btnLogin = (Button) findViewById(R.id.btnlogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txtUserName.length() != 0 && txtPasswd.length() != 0){
+                    if(database.checkLogin(txtUserName.getText().toString().trim(),txtPasswd.getText().toString().trim())){
+                        Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                        i.putExtra("username",txtUserName.getText().toString());
+                        startActivity(i);
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công!!!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thất bại!!!", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin!!!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this,SignUpActiviry.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
